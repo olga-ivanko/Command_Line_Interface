3a4cea8024320b531ff22fa6ac8f0eb09e9246d4
olga-ivanko <o.ivanko@me.com>
2023-10-03 12:51:46 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,110 @@
+import collections
+
+
+Contact = collections.namedtuple('Contact', ['name', 'phone'])
+CONTACTS_LIST = set()
+
+
+
+#raction on hello   
+def func_hello() -> None:
+    print ("How can I help you?")
+    next_func = define_command(user_input=input(">>> "))
+    return next_func
+
+#decorator for errors
+def input_error(func):
+    def inner():
+        try:
+            return func()
+        except IndexError:
+            print(f"Not enough params")
+        
+    return inner
+        
+
+#adding new Contact to the contacts list
+@input_error 
+def func_add():
+    contact_ = input("Enter name and phone: ").upper()
+    name, phone = contact_.split()[0], contact_.split()[1]
+    new_contact = Contact(name, phone)
+    CONTACTS_LIST.add(new_contact)
+    print(f"New contact with name: {new_contact.name} and with number: {new_contact.phone} succesfully added")
+    return get_input()
+
+
+
+#changing the number of existing contact 
+def func_change():
+    input_name = input("Please enter the name of the contact for changing: ").upper()
+    for i in filter(lambda x: x.name == input_name, CONTACTS_LIST):
+       existing_contact = i
+       new_phone = input("Please enter new phone: ")
+       
+    edited_contact = Contact(input_name, new_phone)
+    CONTACTS_LIST.remove(existing_contact)
+    CONTACTS_LIST.add(edited_contact)
+
+    print(f"contact {edited_contact.name} was succesfully updated with the numer: {edited_contact.phone}")  
+
+    return get_input()
+
+#search phone in contacts list
+def func_phone():
+    input_name = input("Please enter the name for search: ").upper()
+    for contact in  CONTACTS_LIST:
+        if input_name == contact.name:
+            print(contact.phone)
+    return get_input()
+
+#show all contacts saved in contacts list 
+def func_show_all():
+    for contact in CONTACTS_LIST:
+        print(f"{contact.name} {contact.phone}")
+    return get_input()
+
+#reaction on exit key word
+def func_good_bye():
+    print("Good bye!")
+    return None
+
+#getting input as param for define_command
+def get_input():
+    user_input = input(">>>  ")
+    return define_command(user_input) 
+
+#the func finds the command from dict by key word
+def define_command(user_input):
+    FUNCTIONS = {"hello" : func_hello,
+            "add" : func_add,
+            "change" : func_change, 
+            "phone" : func_phone,
+            "show all" : func_show_all,
+            "good bye" : func_good_bye, 
+            "close" : func_good_bye, 
+            "exit" : func_good_bye}
+    
+    if user_input.lower() in FUNCTIONS:
+        run_func = FUNCTIONS.get(user_input.lower())()
+        return run_func
+
+    
+
+        
+
+def main() -> str:
+    
+    while True:
+        user_input = input(">>> ").lower()        
+        define_command(user_input)
+        if user_input in ["", "exit", "."]:
+            break
+    return None
+
+    
+
+
+if __name__ == '__main__':
+
+    main()
